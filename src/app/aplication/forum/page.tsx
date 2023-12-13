9886d5e9732568e9591afbc4c9d49a3a049310b1
Cachaza <davidcachazav@gmail.com>
2023-12-13 18:10:06 +0100

    updated icon and images

--- src/app/aplication/forum/page.tsx
+++ src/app/aplication/forum/page.tsx
@@ -39,7 +39,7 @@ const InstagramPostJson = [
     userName: "Shad",
     userImage: "https://avatars.githubusercontent.com/u/11354539?v=4",
     postImage:
-      "https://images.unsplash.com/photo-1627391427637-5c3e7f1f2e4b?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHh6ZjJ3bG5jZ3x8ZW58MHx8fHwxNjMwMzU0MDY1&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "https://hips.hearstapps.com/hmg-prod/images/zaragoza4-1606575915.jpg?resize=980:*",
     postDescription:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas.",
     postComments: [
@@ -61,7 +61,7 @@ const InstagramPostJson = [
     userName: "Shad",
     userImage: "https://avatars.githubusercontent.com/u/11354539?v=4",
     postImage:
-      "https://images.unsplash.com/photo-1627391427637-5c3e7f1f2e4b?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHh6ZjJ3bG5jZ3x8ZW58MHx8fHwxNjMwMzU0MDY1&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "https://files.oaiusercontent.com/file-O31PLOTUsSOm8p9CD3hr6OcC?se=2023-12-13T17%3A48%3A57Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D074bbf41-8f28-4fb2-b315-6808a9b9df90.webp&sig=v5B50w/mxNBD5w30T%2BdBGaumf/Wvuwpyp8xWZg2GYNk%3D",
     postDescription:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas.",
     postComments: [
@@ -83,7 +83,7 @@ const InstagramPostJson = [
     userName: "Shad",
     userImage: "https://avatars.githubusercontent.com/u/11354539?v=4",
     postImage:
-      "https://images.unsplash.com/photo-1627391427637-5c3e7f1f2e4b?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHh6ZjJ3bG5jZ3x8ZW58MHx8fHwxNjMwMzU0MDY1&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "https://cdn.resfu.com/media/img_news/partido-de-futbol-para-discapacitados-en-sierra-leona--afp.jpg?size=1000x&lossy=1",
     postDescription:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas. Quisquam quidem, quibusdam, voluptate, quod voluptatibus fugiat aperiam voluptatum quos voluptas dolorum nesciunt quas.",
     postComments: [
@@ -105,7 +105,7 @@ const InstagramPostJson = [
     userName: "Shad",
     userImage: "https://avatars.githubusercontent.com/u/11354539?v=4",
     postImage:
-      "https://images.unsplash.com/photo-1627391427637-5c3e7f1f2e4b?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHh6ZjJ3bG5jZ3x8ZW58MHx8fHwxNjMwMzU0MDY1&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "https://files.oaiusercontent.com/file-a1hx7BJXWsJZlViOPKtxwjB4?se=2023-12-13T18%3A08%3A58Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D63c4f7fe-779a-4881-986e-87fed8646e38.webp&sig=ezJ2t2nnXDDwYnwodYcu/QpzQX5KPhMRbSKh18%2B8lxU%3D",
     postDescription:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, quibusdam, voluptate",
   },
